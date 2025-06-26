bca02417fbb902ecf3e5e477c62c51710d0474c3
josie <a01033850849@gmail.com>
2025-06-26 16:09:17 +0900

    solve 1,2,3

--- pages/lab1.py
+++ pages/lab1.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'Malgun Gothic'
+
 st.title("lab1")
-url = "https://github.com/myoh0623/dataset/blob/main/student-mat.csv"
+url = "https://raw.githubusercontent.com/myoh0623/dataset/refs/heads/main/student-mat.csv"
 df = pd.read_csv(url)
 
 # 성별 평균 성적 비교(G3 컬럼은 성적을 나타낸다)
@@ -15,7 +18,11 @@ st.bar_chart(gender_score)
 st.subheader("2. 공부 시간에 따른 평균 최종 성적")
 studytime_score = df.groupby("studytime")["G3"].mean()
 
+st.bar_chart(studytime_score)
+
 # walc mean
 
 st.subheader("3. 주말 알콜 섭취에 따른 평균 최종 성적")
-walc_score = df.groupby("walc")["G3"].mean()
+walc_score = df.groupby("Walc")["G3"].mean()
+
+st.bar_chart(walc_score)
